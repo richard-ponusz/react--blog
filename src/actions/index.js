6c4a0dfff36e9869643aba99b4c42bba0a0221a0
richard-ponusz <ponuszrichard93@gmail.com>
2020-06-17 18:08:01 +0200

    fetching users without overfetching

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,11 @@
-// import _ from 'lodash';
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPostsAndUsers = () => async dispatch => {
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
     await dispatch(fetchPosts());
+
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
 }
 
 export const fetchPosts = () => async dispatch => {
@@ -10,6 +13,7 @@ export const fetchPosts = () => async dispatch => {
     
     dispatch({ type: 'FETCH_POSTS', payload: response.data }) 
 };
+
 export const fetchUser = (id) => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
